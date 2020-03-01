01ce881a440b6827c1f431cb17bb4da7c4f13111
hasanboyaci15 <hsnbyc@gmail.com>
2020-03-01 12:35:42 -0600

    BasisNavigation

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         //to start selenium script we need:
         //setup WebDriver (browser driver)and create WebDriver object
         WebDriverManager.chromedriver().setup();
