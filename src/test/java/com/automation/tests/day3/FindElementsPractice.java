07141f647bde26a88ee63334edb1980d68f62fae
hasanboyaci15 <hsnbyc@gmail.com>
2020-03-04 20:25:58 -0600

    BasisNavigation

---
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -0,0 +1,38 @@
+package com.automation.tests.day3;
+
+import com.automation.utilities.DriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElementsPractice {
+    public static void main(String[] args) {
+
+//        WebDriverManager.chromedriver().setup();
+//        WebDriver driver=new ChromeDriver();
+
+        WebDriver driver= DriverFactory.createADriver("chrome");
+        driver.get("http://practice.cybertekschool.com/sign_up");
+        WebElement fullName=driver.findElement(By.name("full_name"));
+        fullName.sendKeys("Mister Twister");
+
+        WebElement email =driver.findElement(By.name("email"));
+        email.sendKeys("sdet@cybertek.com");
+
+        WebElement signUP=driver.findElement(By.name("wooden_spoon"));
+
+        signUP.submit();
+
+
+        driver.quit();
+
+
+
+
+
+
+
+    }
+}
---
+++ src/test/java/com/automation/utilities/DriverFactory.java
@@ -0,0 +1,26 @@
+package com.automation.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+    /**
+     * This method returns webdriver object based on browser type
+     * If you want to use chrome browser, just provide chore as parameter
+     * @param browserName
+     * @return
+     */
+    public static WebDriver createADriver(String browserName){
+        if (browserName.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+
+        }else {
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+
+        }
+    }
+}
